cf0ecee59419d14c0f1adf04462013a00970621a
Umaan-student <umantomail@gmail.com>
2025-10-04 08:35:30 +0530

    Update1 first.c

--- first.c
+++ first.c
@@ -1,9 +1,9 @@
 //written in turbo c++
-#include <studio.h>
+#include <stdio.h>
 #include <conio.h>
 void main(){
 clrscr();
-printf("BCA FY");
-printf("Abra ka dabra");
+printf("BCA FY\n");
+printf("Abra ka dabra\n");
 getch();
 }
