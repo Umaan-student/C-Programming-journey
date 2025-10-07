469f52aa05822c25f73a3972cf2449fe987a9db8
Umaan-student <umantomail@gmail.com>
2025-10-07 11:04:44 +0530

    Create add two number.c

---
+++ add two number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main() {
+    clrscr();
+    int digit1 = 10;
+    int digit2 = 10;
+    int sum = digit1+digit2;
+    printf("%d",sum);
+    getch();
+}
