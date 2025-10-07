9f6ba09f72a77fdfed9330b9f1abe32b31512ead
Umaan-student <umantomail@gmail.com>
2025-10-07 09:01:21 +0530

    Create addtwono.c

---
+++ addtwono.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main() {
+    clrscr();
+    int a,b,sum;
+    printf("first value=");
+    scanf("%d", &a);
+    printf("second  value=");
+    scanf("%d", &b);
+    sum= a+b;
+    printf("addition=%d", sum);
+    getch();
+}
